3179ccfe36814503a78b145811a8240d0a19c8a5
Mateo Radman <48420316+mateoradman@users.noreply.github.com>
2023-04-23 17:02:22 +0200

    add unit tests for files

--- src/files.rs
+++ src/files.rs
@@ -19,3 +19,33 @@ pub fn get_destination_path(filename: &str) -> String {
     let dest_path = cwd.join(new_filename);
     dest_path.into_os_string().into_string().unwrap()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_available_filename() {
+        let filename = "bla";
+        let available = get_available_filename(filename);
+        assert_eq!(filename, &available);
+    }
+    #[test]
+    fn test_get_available_filename_appends_timestamp() {
+        let cwd = env::current_dir().unwrap();
+        let cwd_as_string = cwd.to_str().unwrap();
+        let available = get_available_filename(cwd_as_string);
+        assert!(available.len() > cwd_as_string.len());
+        assert!(available.contains(cwd_as_string));
+    }
+
+    #[test]
+    fn test_get_destination_path() {
+        let filename = "bla";
+        let cwd = env::current_dir().unwrap();
+        let path = cwd.join(filename);
+        let expected_result = path.to_str().unwrap();
+        let dest_path = get_destination_path(filename);
+        assert_eq!(expected_result, dest_path);
+    }
+}
