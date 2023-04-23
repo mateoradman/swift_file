a3363bdd7884d481d1de69191606631fc48e10ae
Mateo Radman <48420316+mateoradman@users.noreply.github.com>
2023-04-23 10:48:43 +0200

    move qr code generation to qr.rs

---
+++ src/qr.rs
@@ -0,0 +1,16 @@
+use local_ip_address::local_ip;
+use qrcode::render::unicode;
+use qrcode::QrCode;
+
+pub fn generate_qr_code(port: u16, route: &str) {
+    let machine_ip = local_ip().unwrap().to_string();
+    let complete_url = format!("http://{machine_ip}:{port}{route}");
+    println!("URL: {complete_url}");
+    let code = QrCode::new(complete_url).unwrap();
+    let image = code
+        .render::<unicode::Dense1x2>()
+        .dark_color(unicode::Dense1x2::Light)
+        .light_color(unicode::Dense1x2::Dark)
+        .build();
+    println!("{}", image);
+}
