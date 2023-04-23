9c6a451604293982773e62ce2560917f38296251
Mateo Radman <48420316+mateoradman@users.noreply.github.com>
2023-04-23 16:00:18 +0200

    Improve CLI arg descriptions

--- src/cli.rs
+++ src/cli.rs
@@ -13,16 +13,16 @@ pub struct Cli {
 pub enum Commands {
     /// Send a file
     Send {
-        /// File to send
+        /// File path to send
         file: PathBuf,
         #[arg(short, long, value_parser = port_in_range)]
-        /// Port on which the server will run (8000-9000)
+        /// Port to bind the server to (allowed user port range 1024 to 49151)
         port: Option<u16>,
     },
     /// Receive a file
     Receive {
         #[arg(short, long, value_parser = port_in_range)]
-        /// Port on which the server will run (8000-9000)
+        /// Port to bind the server to (allowed user port range 1024 to 49151)
         port: Option<u16>,
     },
 }
