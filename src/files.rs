bfa794a3f546810f0eddf6f0f981c7eccb9c2c14
Mateo Radman <48420316+mateoradman@users.noreply.github.com>
2023-04-25 07:42:45 +0200

    restructure the code a bit and add possibility of receiving a file in a custom dir

--- src/files.rs
+++ src/files.rs
@@ -2,21 +2,25 @@ use std::{env, path::PathBuf};
 
 use chrono::Local;
 
-fn get_available_filename(filename: &str) -> String {
-    let now = Local::now();
-    let now = now.format("%d-%m-%Y %H:%M:%S").to_string();
-    let path = PathBuf::from(filename);
+fn set_available_filename(path: &mut PathBuf) {
     if path.is_dir() || path.is_file() {
-        let new_filename = format!("{now} - {filename}");
-        return new_filename;
+        let now = Local::now().format("%d-%m-%Y %H:%M:%S").to_string();
+        let new_filename = match path.file_name() {
+            Some(filename) => format!("{now} - {}", filename.to_str().unwrap()),
+            None => String::from("uploaded file"),
+        };
+        path.set_file_name(new_filename);
     };
-    filename.to_string()
 }
 
-pub fn get_destination_path(filename: &str) -> String {
-    let new_filename = get_available_filename(filename);
+pub fn get_destination_path(filename: &str, destination_dir: &Option<PathBuf>) -> String {
     let cwd = env::current_dir().unwrap();
-    let dest_path = cwd.join(new_filename);
+    let working_dir = match destination_dir {
+        Some(dest_dir) => dest_dir,
+        None => &cwd,
+    };
+    let mut dest_path = working_dir.join(filename);
+    set_available_filename(&mut dest_path);
     dest_path.into_os_string().into_string().unwrap()
 }
 
@@ -26,17 +30,16 @@ mod tests {
 
     #[test]
     fn test_get_available_filename() {
-        let filename = "bla";
-        let available = get_available_filename(filename);
-        assert_eq!(filename, &available);
+        let mut filename = PathBuf::from("bla");
+        set_available_filename(&mut filename);
+        assert_eq!(filename, PathBuf::from("bla"));
     }
     #[test]
     fn test_get_available_filename_appends_timestamp() {
+        let mut working_dir = env::current_dir().unwrap();
+        set_available_filename(&mut working_dir);
         let cwd = env::current_dir().unwrap();
-        let cwd_as_string = cwd.to_str().unwrap();
-        let available = get_available_filename(cwd_as_string);
-        assert!(available.len() > cwd_as_string.len());
-        assert!(available.contains(cwd_as_string));
+        assert!(working_dir.file_name() != cwd.file_name());
     }
 
     #[test]
@@ -45,7 +48,7 @@ mod tests {
         let cwd = env::current_dir().unwrap();
         let path = cwd.join(filename);
         let expected_result = path.to_str().unwrap();
-        let dest_path = get_destination_path(filename);
+        let dest_path = get_destination_path(filename, &Some(cwd));
         assert_eq!(expected_result, dest_path);
     }
 }
