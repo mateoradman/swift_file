0b3630a60d74e339e90cda03b13afe69a1651785
mateo <mateo.radman@t3k.ai>
2023-05-10 14:30:16 +0200

    improve receive endpoint and print human readable size when a file is received

--- src/qr.rs
+++ src/qr.rs
@@ -1,3 +1,4 @@
+use crate::network::LOCALHOST;
 use local_ip_address::local_ip;
 use qrcode::render::unicode;
 use qrcode::QrCode;
@@ -5,12 +6,13 @@ use qrcode::QrCode;
 pub fn generate_qr_code(port: &u16, route: &str) {
     let machine_ip = local_ip().unwrap().to_string();
     let complete_url = format!("http://{machine_ip}:{port}{route}");
-    println!("URL: {complete_url}");
-    let code = QrCode::new(complete_url).unwrap();
+    let code = QrCode::new(&complete_url).unwrap();
     let image = code
         .render::<unicode::Dense1x2>()
         .dark_color(unicode::Dense1x2::Light)
         .light_color(unicode::Dense1x2::Dark)
         .build();
-    println!("{}", image);
+    println!("{}\n", image);
+    println!("Server running on http://{LOCALHOST}:{port}");
+    println!("QR Code URL: {complete_url}");
 }
--- src/receive.rs
+++ src/receive.rs
@@ -1,3 +1,4 @@
+use humansize::{format_size, DECIMAL};
 use std::fs::write;
 
 use axum::{
@@ -76,31 +77,47 @@ async fn accept_form(State(state): State<AppState>, mut multipart: Multipart) ->
         let file_name = field.file_name().unwrap().to_string();
         let content_type = field.content_type().unwrap().to_string();
         let data = field.bytes().await.unwrap();
+        let human_size: String = format_size(data.len(), DECIMAL);
         let dest_path = get_destination_path(&file_name, &state.destination_dir);
-        match write(&dest_path, &data) {
-            Ok(_) => println!(
-                "File `{}` with content type `{}`, size {} bytes has been transferred to {}",
-                file_name,
-                content_type,
-                data.len(),
-                dest_path
-            ),
-            Err(err) => println!(
-                "Unable to transfer file {} with content type {} and size {} to {} due to {}",
-                file_name,
-                content_type,
-                data.len(),
-                dest_path,
-                err
-            ),
-        }
-
-        match opener::open(&dest_path) {
-            Ok(()) => println!("File opened using a default program."),
+        let written: bool = match write(&dest_path, &data) {
+            Ok(_) => {
+                println!(
+                    "Successfully transferred {}",
+                    construct_file_info(&file_name, &content_type, &human_size, &dest_path),
+                );
+                true
+            }
             Err(err) => {
-                println!("Unable to open a file using the system default program due to {err}")
+                println!(
+                    "Unable to transfer {} due to `{}`",
+                    construct_file_info(&file_name, &content_type, &human_size, &dest_path),
+                    err
+                );
+                false
+            }
+        };
+
+        if written {
+            match opener::open(&dest_path) {
+                Ok(()) => println!("File opened using a system default program."),
+                Err(err) => {
+                    println!("Unable to open a file using the system default program due to {err}")
+                }
             }
         }
     }
+
     Redirect::to("/receive")
+}
+
+fn construct_file_info(
+    file_name: &str,
+    content_type: &str,
+    human_size: &str,
+    destination_path: &str,
+) -> String {
+    format!(
+        "file `{}` with content type `{}` and size `{}` to `{}`",
+        file_name, content_type, human_size, destination_path
+    )
 }
